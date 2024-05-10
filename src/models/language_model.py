d90d815612b2368e8d85620a7b9fde511ebba0af
Noah <pfnoah@ethz.ch>
2024-05-10 15:06:02 +0200

    transformer models added, llama pending

--- src/models/language_model.py
+++ src/models/language_model.py
@@ -4,6 +4,7 @@ from enum import Enum
 import numpy as np
 from tqdm import tqdm
 import transformers
+from transformers import BertTokenizer, BertModel, GPT2Tokenizer, GPT2Model, LlamaTokenizer, LlamaModel
 
 from src.data.datatypes import TextData, EncodingData
 from src.data.download import Downloader
@@ -57,3 +58,43 @@ class GloveLanguageModel(LanguageModel):
             embeddings.append(self.aggregation(text_embedding, axis=0))
         return embeddings
 
+#---------------------Transformers---------------------
+
+class BertLanguageModel(LanguageModel):
+    def __init__(self, model_name='bert-base-uncased'):
+        self.tokenizer = BertTokenizer.from_pretrained(model_name)
+        self.model = BertModel.from_pretrained(model_name)
+
+    def _encode(self, texts: TextData) -> EncodingData:
+        encodings = []
+        for text in texts:
+            encoded_input = self.tokenizer(text, return_tensors='pt')
+            output = self.model(**encoded_input)
+            encodings.append(output.last_hidden_state.mean(dim=1).detach().numpy())
+        return encodings
+
+class GPT2LanguageModel(LanguageModel):
+    def __init__(self, model_name='gpt2'):
+        self.tokenizer = GPT2Tokenizer.from_pretrained(model_name)
+        self.model = GPT2Model.from_pretrained(model_name)
+
+    def _encode(self, texts: TextData) -> EncodingData:
+        encodings = []
+        for text in texts:
+            encoded_input = self.tokenizer(text, return_tensors='pt')
+            output = self.model(**encoded_input)
+            encodings.append(output.last_hidden_state.mean(dim=1).detach().numpy())
+        return encodings
+
+class LlamaLanguageModel(LanguageModel):
+    def __init__(self, model_name='Llama-2-7b-chat-hf'): #no access to repository yet
+        self.tokenizer = LlamaTokenizer.from_pretrained(model_name)
+        self.model = LlamaModel.from_pretrained(model_name)
+
+    def _encode(self, texts: TextData) -> EncodingData:
+        encodings = []
+        for text in texts:
+            encoded_input = self.tokenizer(text, return_tensors='pt')
+            output = self.model(**encoded_input)
+            encodings.append(output.last_hidden_state.mean(dim=1).detach().numpy())
+        return encodings
