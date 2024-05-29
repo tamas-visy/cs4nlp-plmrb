beabf21e07d1b1c9e9aeaaee2fc4bfe20031d236
Susanna Di Vita <susanna.divita.2@gmail.com>
2024-05-29 09:10:49 +0000

    Previou script, incomplete, iterativer additions for final testing

--- src/data/trans.py
+++ src/data/trans.py
@@ -2,16 +2,13 @@ import re
 import nltk
 from nltk.tokenize import word_tokenize
 from nltk import pos_tag, ne_chunk
-import spacy
 
 nltk.download('punkt')
 nltk.download('maxent_ne_chunker')
 nltk.download('words')
 nltk.download('averaged_perceptron_tagger')
 
-religion_filters = {"Christianity", "Islam", "Judaism", "Hinduism", "Buddhism"}
-race_filters = {"Asian", "Black", "White", "Hispanic", "Latino", "Native American"}
-gender_filters = {"male", "female", "non-binary", "transgender"}
+gender_filters = {"male", "female", "non-binary", "transgender", "transsexual", "LGBTQ", "queer"}
 
 def string_match_filter(text, filters):
     matches = []
@@ -29,57 +26,35 @@ def ner_filter(text):
         if isinstance(entity, nltk.tree.Tree):
             if entity.label() == 'PERSON':
                 processed_sentence.append('[NAME]')
-            elif entity.label() == 'GPE':
-                processed_sentence.append('')
             else:
-                entity_text = ' '.join(word for word, tag in entity.leaves())
-                if any(keyword in entity_text.lower() for keyword in ['nation', 'country', 'religion', 'religious']):
-                    processed_sentence.append('')
-                else:
-                    processed_sentence.extend([word for word, tag in entity])
+                processed_sentence.extend([word for word, tag in entity])
         else:
             processed_sentence.append(entity[0])
     return ' '.join(processed_sentence)
 
-nlp = spacy.load("en_core_web_sm")
-
-def ner_filter_spacy(text):
-    doc = nlp(text)
-    nationalities = []
-    for ent in doc.ents:
-        if ent.label_ == "NORP":
-            nationalities.append(ent.text)
-        elif ent.label_ == "GPE":
-            if ent.text.istitle():
-                if ent.text.istitle():
-                    nationalities.add(ent.text)
-    return nationalities
-
 def process_sentence(sentence):
     processed_sentence = ner_filter(sentence)
-    religion_matches = string_match_filter(processed_sentence, religion_filters)
+    
     gender_matches = string_match_filter(processed_sentence, gender_filters)
-    religion_ner_matches = ner_filter(processed_sentence)
+    
     gender_ner_matches = ner_filter(processed_sentence)
-    nationalities_ner_spacy = ner_filter_spacy(sentence)
 
-    print("String matching for religion:", religion_matches)
-    print("NER for religion:", religion_ner_matches)
-    print("String matching for gender:", gender_matches)
-    print("NER for gender:", gender_ner_matches)
-    print("NER for nationalities (using spaCy):", nationalities_ner_spacy)
-    print()
+    if '[NAME]' in processed_sentence:
+        print("Processed sentence:", processed_sentence)
+        print("String matching for gender:", gender_matches)
+        print("NER for gender:", gender_ner_matches)
+        print()
 
 example_sentences = [
-    "The Christian community celebrated Easter.",
-    "Asian and Black individuals participated in the event.",
     "A female transgender person gave a speech.",
     "John Smith attended the conference.",
-    "The Hindu festival is celebrated with great enthusiasm.",
-    "Sheikh Mohammed donated a large sum of money to the mosque.",
-    "White supremacists marched through the streets.",
-    "The Latino population is growing rapidly.",
-    "Transgender rights activists protested outside the courthouse."
+    "Transgender rights activists protested outside the courthouse.",
+    "The non-binary community celebrated Pride Month with various events.",
+    "She identifies as queer and is an advocate for LGBTQ rights.",
+    "The male participants were enthusiastic about the workshop.",
+    "A transsexual woman shared her story at the event.",
+    "The LGBTQ community held a march in support of equal rights.",
+    "He came out as gay during the meeting."
 ]
 
 for sentence in example_sentences:
