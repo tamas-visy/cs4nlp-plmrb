72583238297bd93e25377b60617fc6512e7280f5
Susanna Di Vita <susanna.divita.2@gmail.com>
2024-05-29 11:12:07 +0200

    Previou script, incomplete, iterativer additions for final testing

--- src/data/s_cleaner.py
+++ src/data/s_cleaner.py
@@ -1,3 +1,4 @@
+import json
 import re
 import nltk
 from nltk.tokenize import word_tokenize
@@ -8,14 +9,40 @@ nltk.download('maxent_ne_chunker')
 nltk.download('words')
 nltk.download('averaged_perceptron_tagger')
 
-religion_filters = {"Christianity", "Islam", "Judaism", "Hinduism", "Buddhism"}
-race_filters = {"Asian", "Black", "White", "Hispanic", "Latino", "Native American"}
-gender_filters = {"male", "female", "non-binary", "transgender"}
+# Word list taken from:
+# https://github.com/gender-bias/gender-bias/tree/master/genderbias: job-posting-specific male and female attributes
+# https://github.com/davidemiceli/gender-detection/: personal names
+# https://github.com/microsoft/responsible-ai-toolbox-genbit/tree/main: gendered professions and adjectives related to cis, non-binary and trans people
+# https://github.com/tolga-b/debiaswe/tree/master: general male and female coded words (implicit gender bias descriptors) and professions
+# TO DEBUG: superlatives only used for women/men, expand on unnecessarly citing personal infomration or gender references when not required, expand on jobs dataset, replace to neutral counterpart
+# WHAT DOESN'T MAKE SENSE: only ocnsidering adjectives that are present in more than 2 lists - too occlusive
+
+def read_names(file_path):
+    names = set()
+    with open(file_path, 'r') as file:
+        for line in file:
+            matches = re.findall(r'"([^"]+)"', line)
+            if matches:
+                names.update(matches)
+    return [name.lower() for name in names]
+
+female_names = read_names("/workspaces/cs4nlp-plmrb/data/raw/female.js")
+male_names = read_names("/workspaces/cs4nlp-plmrb/data/raw/male.js")
+
+def read_wordlist(file_path):
+    with open(file_path, 'r') as file:
+        return {word.strip() for word in file}
+
+female_attributes_filters = read_wordlist("/workspaces/cs4nlp-plmrb/data/raw/female_adjectives.wordlist")
+male_attributes_filters = read_wordlist("/workspaces/cs4nlp-plmrb/data/raw/male_adjectives.wordlist")
+female_jobs_filters = read_wordlist("/workspaces/cs4nlp-plmrb/data/raw/female.txt")
+male_jobs_filters = read_wordlist("/workspaces/cs4nlp-plmrb/data/raw/male.txt")
 
 def string_match_filter(text, filters):
     matches = []
     for filter_word in filters:
-        if re.search(r'\b{}\b'.format(re.escape(filter_word)), text, re.IGNORECASE):
+        pattern = re.compile(r'\b{}\w*\b'.format(re.escape(filter_word)), re.IGNORECASE)
+        if re.search(pattern, text):
             matches.append(filter_word)
     return matches
 
@@ -28,49 +55,63 @@ def ner_filter(text):
         if isinstance(entity, nltk.tree.Tree):
             if entity.label() == 'PERSON':
                 processed_sentence.append('[NAME]')
-            elif entity.label() == 'GPE':  
-                processed_sentence.append('') 
             else:
-                entity_text = ' '.join(word for word, tag in entity.leaves())
-                if any(keyword in entity_text.lower() for keyword in ['nation', 'country', 'religion', 'religious']):
-                    processed_sentence.append('') 
-                else:
-                    processed_sentence.extend([word for word, tag in entity])
+                entity_words = []
+                for word, tag in entity.leaves():
+                    if word.lower() in female_names:
+                        entity_words.append('[FEMALE NAME]')
+                    elif word.lower() in male_names:
+                        entity_words.append('[MALE NAME]')
+                    elif word.lower() in female_jobs_filters:
+                        entity_words.append('[FEMALE JOB]')
+                    elif word.lower() in male_jobs_filters:
+                        entity_words.append('[MALE JOB]')
+                    for attribute_filter in female_attributes_filters:
+                        pattern = re.compile(r'\b{}\w*\b'.format(re.escape(attribute_filter)), re.IGNORECASE)
+                        if re.match(pattern, word):
+                            entity_words.append('[FEMALE ATTRIBUTES]')
+                            break
+                    for attribute_filter in male_attributes_filters:
+                        pattern = re.compile(r'\b{}\w*\b'.format(re.escape(attribute_filter)), re.IGNORECASE)
+                        if re.match(pattern, word):
+                            entity_words.append('[MALE ATTRIBUTES]')
+                            break
+                    else:
+                        entity_words.append(word)
+                processed_sentence.extend(entity_words)
         else:
             processed_sentence.append(entity[0])
     return ' '.join(processed_sentence)
 
-
 def process_sentence(sentence):
     processed_sentence = ner_filter(sentence)
-    # print("Processed sentence:", processed_sentence) # used just for personal name masking checks
     
-    religion_matches = string_match_filter(processed_sentence, religion_filters)
-    race_matches = string_match_filter(processed_sentence, race_filters)
-    gender_matches = string_match_filter(processed_sentence, gender_filters)
-
-    religion_ner_matches = ner_filter(processed_sentence)
-    race_ner_matches = ner_filter(processed_sentence)
+    gender_matches = string_match_filter(processed_sentence, female_attributes_filters)
+    
     gender_ner_matches = ner_filter(processed_sentence)
 
-    print("String matching for religion:", religion_matches)
-    print("NER for religion:", religion_ner_matches)
-    print("String matching for race:", race_matches)
-    print("NER for race:", race_ner_matches)
-    print("String matching for gender:", gender_matches)
-    print("NER for gender:", gender_ner_matches)
-    print()
+    if '[NAME]' or '[FEMALE ATTRIBUTES]' or '[MALE ATTRIBUTES]' or '[FEMALE NAME]' or '[MALE NAME]' or '[FEMALE JOB]' or '[MALE JOB]' in processed_sentence:
+        #print("Processed sentence:", processed_sentence)
+        print("String matching for gender:", gender_matches)
+        #print("NER for gender:", gender_ner_matches)
+        print()
 
 example_sentences = [
-    "The Christian community celebrated Easter.",
-    "Asian and Black individuals participated in the event.",
     "A female transgender person gave a speech.",
     "John Smith attended the conference.",
-    "The Hindu festival is celebrated with great enthusiasm.",
-    "Sheikh Mohammed donated a large sum of money to the mosque.",
-    "White supremacists marched through the streets.",
-    "The Latino population is growing rapidly.",
-    "Transgender rights activists protested outside the courthouse."
+    "Transgender rights activists protested outside the courthouse.",
+    "The non-binary community celebrated Pride Month with various events.",
+    "She identifies as queer and is an advocate for LGBTQ rights.",
+    "The male participants were enthusiastic about the workshop.",
+    "A transsexual woman shared her story at the event.",
+    "The LGBTQ community held a march in support of equal rights.",
+    "He came out as gay during the meeting.",
+    "Anna is gentle and caring towards others.",
+    "His hardworking nature led to his success in the project.",
+    "She is honest and forthright in her opinions.",
+    "He has excellent interpersonal skills, making him a great team player.",
+    "The success of the project is due to their interdependence.",
+    "vidya is a kind and compassionate bro."
 ]
 
 for sentence in example_sentences:
