4c9ee1e9a44ea08d3edc6d18da317e546c698d26
susannadiv <susanna.divita.2@gmail.com>
2024-06-12 22:50:33 +0200

    Correct ds

--- src/data/dropping.py
+++ src/data/dropping.py
@@ -32,17 +32,17 @@ def read_names(file_path):
                 names.update(matches)
     return [name.lower() for name in names]
 
-female_names = read_names("/workspaces/cs4nlp-plmrb/data/raw/female.js")
-male_names = read_names("/workspaces/cs4nlp-plmrb/data/raw/male.js")
-male_titles = read_wordlist("/workspaces/cs4nlp-plmrb/data/raw/male_word_file.txt")
-female_titles = read_wordlist("/workspaces/cs4nlp-plmrb/data/raw/female_word_file.txt")
-female_jobs_filters = read_wordlist("/workspaces/cs4nlp-plmrb/data/raw/female.txt")
-male_jobs_filters = read_wordlist("/workspaces/cs4nlp-plmrb/data/raw/male.txt")
-cis = read_wordlist("/workspaces/cs4nlp-plmrb/data/raw/cis.txt")
-trans = read_wordlist("/workspaces/cs4nlp-plmrb/data/raw/trans.txt")
-non_binary = read_wordlist("/workspaces/cs4nlp-plmrb/data/raw/non-binary.txt")
-female_attributes_filters = read_wordlist("/workspaces/cs4nlp-plmrb/data/raw/female_adjectives.wordlist")
-male_attributes_filters = read_wordlist("/workspaces/cs4nlp-plmrb/data/raw/male_adjectives.wordlist")
+female_names = read_names(r"C:\Users\susan\.csnlp\cs4nlp-plmrb\data\raw\female.js")
+male_names = read_names(r"C:\Users\susan\.csnlp\cs4nlp-plmrb\data\raw\male.js")
+male_titles = read_wordlist(r"C:\Users\susan\.csnlp\cs4nlp-plmrb\data\raw\male_word_file.txt")
+female_titles = read_wordlist(r"C:\Users\susan\.csnlp\cs4nlp-plmrb\data\raw\female_word_file.txt")
+female_jobs_filters = read_wordlist(r"C:\Users\susan\.csnlp\cs4nlp-plmrb\data\raw\female.txt")
+male_jobs_filters = read_wordlist(r"C:\Users\susan\.csnlp\cs4nlp-plmrb\data\raw\male.txt")
+cis = read_wordlist(r"C:\Users\susan\.csnlp\cs4nlp-plmrb\data\raw\cis.txt")
+trans = read_wordlist(r"C:\Users\susan\.csnlp\cs4nlp-plmrb\data\raw\trans.txt")
+non_binary = read_wordlist(r"C:\Users\susan\.csnlp\cs4nlp-plmrb\data\raw\non-binary.txt")
+female_attributes_filters = read_wordlist(r"C:\Users\susan\.csnlp\cs4nlp-plmrb\data\raw\female_adjectives.wordlist")
+male_attributes_filters = read_wordlist(r"C:\Users\susan\.csnlp\cs4nlp-plmrb\data\raw\male_adjectives.wordlist")
 male_jobs_filters = [word for word in male_jobs_filters if word not in cis and word not in trans and word not in non_binary]
 female_jobs_filters = [word for word in female_jobs_filters if word not in cis and word not in trans and word not in non_binary]
 
@@ -85,7 +85,7 @@ def process_sentence(sentence):
     gender_matches_orientation = string_match_filter(processed_sentence, set(trans).union(cis).union(non_binary), 'GENDERED ORIENTATION')
     gender_matches_titles = string_match_filter(processed_sentence, set(female_titles).union(male_titles), 'GENDERED TITLES')
 
-    processed_sentence = string_match_filter(processed_sentence, set(trans).union(cis).union(non_binary), 'GENDERED ORIENTATION')
+    processed_sentence = string_match_filter(processed_sentence, set(trans).union(non_binary), 'GENDERED ORIENTATION')
     processed_sentence = string_match_filter(processed_sentence, set(female_titles).union(male_titles), 'GENDERED TITLES')
     processed_sentence = string_match_filter(processed_sentence, set(female_attributes_filters).union(male_attributes_filters), 'GENDERED ATTRIBUTES')
     processed_sentence = string_match_filter(processed_sentence, set(female_jobs_filters).union(male_jobs_filters), 'GENDERED JOBS')
@@ -94,6 +94,7 @@ def process_sentence(sentence):
 
 
     if '[NAME]' or '[FEMALE ATTRIBUTES]' or '[MALE ATTRIBUTES]' or '[FEMALE NAME]' or '[MALE NAME]' or '[FEMALE JOB]' or '[MALE JOB]' in processed_sentence:
+        print("Processing sentence:", sentence)
         print("Processed sentence:", processed_sentence)
         #print("String matching for gender attributes:", gender_matches_attributes)
         #print("String matching for gender jobs:", gender_matches_jobs)
@@ -101,28 +102,7 @@ def process_sentence(sentence):
         #print("String matching for gender orientation or pronouns:", gender_matches_orientation)
         #print("String matching for gender titles:", gender_matches_titles)
 
-example_sentences = [
-    "A female transgender person gave a speech.",
-    "John Smith attended the conference.",
-    "Transgender rights activists protested outside the courthouse.",
-    "The non-binary community celebrated Pride Month with various events.",
-    "She identifies as queer and is an advocate for LGBTQ rights.",
-    "The male participants were enthusiastic about the workshop.",
-    "A transsexual woman shared her story at the event.",
-    "The LGBTQ community held a march in support of equal rights.",
-    "He came out as gay during the meeting.",
-    "Anna is gentle and caring towards others.",
-    "His hardworking nature led to his success in the project.",
-    "She is honest and forthright in her opinions.",
-    "He has excellent interpersonal skills, making him a great team player.",
-    "The success of the project is due to their interdependence.",
-    "annabel is a kind and compassionate waiter.",
-    "drag queens pay more taxes to marco"
-]
 
-for sentence in example_sentences:
-    print("Processing sentence:", sentence)
-    process_sentence(sentence)
 
 def common_words(*lists):
     word_counts = defaultdict(int)
