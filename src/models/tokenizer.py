9055f57a7c3e68cef2e6be7cb876e28f529714c3
Vansh Gupta <vanshg2001@protonmail.com>
2024-06-13 12:56:21 +0530

    refactor: Add clean_tokens method to Tokenizer class

--- src/models/tokenizer.py
+++ src/models/tokenizer.py
@@ -1,7 +1,27 @@
 import nltk
-
+import re
 
 class Tokenizer:
     @classmethod
     def tokenize(cls, text):
-        return nltk.word_tokenize(text)
+        cleaned_text = cls.clean_tokens(text)
+        return nltk.word_tokenize(cleaned_text)
+
+
+    @classmethod
+    def clean_tokens(cls, string):
+        """Originally taken from https://github.com/yoonkim/CNN_sentence/blob/master/process_data.py"""
+        string = re.sub(r"[^A-Za-z0-9(),!?\'\`]", " ", string)
+        string = re.sub(r"\'s", " \'s", string)
+        string = re.sub(r"\'ve", " \'ve", string)
+        string = re.sub(r"n\'t", " n\'t", string)
+        string = re.sub(r"\'re", " \'re", string)
+        string = re.sub(r"\'d", " \'d", string)
+        string = re.sub(r"\'ll", " \'ll", string)
+        string = re.sub(r",", " , ", string)
+        string = re.sub(r"!", " ! ", string)
+        string = re.sub(r"\(", " \( ", string)
+        string = re.sub(r"\)", " \) ", string)
+        string = re.sub(r"\?", " \? ", string)
+        string = re.sub(r"\s{2,}", " ", string)
+        return string.strip()
